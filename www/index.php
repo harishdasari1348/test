207f1c1893e9f37736afe949daebcfc0293cfcf6
harishdasari1348 <harish.dasari5@gmail.com>
2017-07-05 18:53:22 +0530

    fdf

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "192.168.99.100";
+$username = "root";
+$password = "docker";
+
+// Create connection
+//this is a comment added by lakshmi
+//this is another comment line by lakshmi
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected \n successfully";
+h  i  
+?>
